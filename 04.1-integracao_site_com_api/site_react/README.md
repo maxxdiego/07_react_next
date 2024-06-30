b04c03d3d827c63ef2c7e834dd95a8ca097325d8
maxx.diego <77diego.max@gmail.com>
2024-06-30 15:50:42 -0300

    @maxxdiego

---
+++ 04.1-integracao_site_com_api/site_react/README.md
@@ -0,0 +1,24 @@
+SEQUÊNCIA PARA CRIAR UM PROJETO REACT
+### npx create-next-app
+
+Acessar o diretório do PROJETO
+### cd [nome-da-pasta]
+
+Rodar o projeto React
+### npm run dev
+
+COMO RODAR O PROJETO BAIXADO
+Instalar todas as dependências indicadas pelo package.json
+### npm install
+
+Rodar o projeto React
+### npm run dev
+
+Acessar no navegador
+### http://localhost:3000
+
+Dependência de ícones
+### npm install react-icons --save
+
+Axios é uma biblioteca de client HTTP que permite fazer requisições a API
+### npm install axios
---
+++ 04.1-integracao_site_com_api/site_react/src/pages/about.js
@@ -0,0 +1,22 @@
+import Head from "next/head"
+import Menu from "@/components/Menu"
+import AboutContent from "@/components/AboutContent"
+import Footer from "@/components/Footer"
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Neotech</title>
+        <meta name="description" content="Landing page com React" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Menu />
+        <AboutContent />
+      </main> 
+      <Footer />
+    </>
+  );
+}
