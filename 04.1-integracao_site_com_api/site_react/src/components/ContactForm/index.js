a437165319f790646891f4620507eb0053767068
maxx.diego <77diego.max@gmail.com>
2024-06-30 19:03:29 -0300

    @maxxdiego

--- 04.1-integracao_site_com_api/site_react/src/components/ContactForm/index.js
+++ 04.1-integracao_site_com_api/site_react/src/components/ContactForm/index.js
@@ -1,6 +1,8 @@
 import styles from '@/components/ContactForm/ContactForm.module.css'
 import { useState } from 'react'
 import axios from 'axios'
+//Biblioteca para validar os campos do formulário
+import * as yup from 'yup'
 
 const ContactForm = ({contentsContacts}) => {
 
@@ -32,6 +34,9 @@ const ContactForm = ({contentsContacts}) => {
         // Bloquear carregamento da página ao enviar o formulário
         e.preventDefault()
         console.log(data)
+
+        // Chamar a função validar o formulário
+        if(!(await validate())) return
 
         // Criar a constante com os dados do cabeçalho
         const headers = {
@@ -89,6 +94,39 @@ const ContactForm = ({contentsContacts}) => {
           })
     }
 
+    // Função validar o formulário
+    async function validate(){
+        let schema = yup.object().shape({
+            content: yup.string("Erro: Necessário preencher o campo conteúdo!")
+            .required("Erro: Necessário preencher o campo conteúdo!"),
+            subject: yup.string("Erro: Necessário preencher o campo assunto!")
+            .required("Erro: Necessário preencher o campo assunto!"),
+            email: yup.string("Erro: Necessário preencher o campo e-mail!")
+            .required("Erro: Necessário preencher o campo e-mail!")
+            .email("Erro: Necessário preencher e-mail válido! "),
+            name: yup.string("Erro: Necessário preencher o campo nome!")
+            .required("Erro: Necessário preencher o campo nome!")
+        })
+
+        try{
+            await schema.validate({
+                name: data.name,
+                email: data.email,
+                subject: data.subject,
+                content: data.content, 
+            })
+            return true
+        } catch(error) {
+            // Alterar formSave para FALSE indicando que pode ativar o botão e atribuir o tipo da mensagem e a mensagem
+            setMessage({
+                formSave: false,
+                type: 'error',
+                message: error.errors
+            })
+            return false;
+        }
+    }
+
     return(
         <div className={`${styles.column} ${styles.right}`}>
             <div className={styles.text}>
@@ -101,19 +139,19 @@ const ContactForm = ({contentsContacts}) => {
             <form onSubmit={sendMsg}>
                 <div className={styles.fields}>
                     <div className={`${styles.field} ${styles.name}`}>
-                        <input type="text" name="name" placeholder="Digite o nome" onChange={valueInput} value={data.name} required/>
+                        <input type="text" name="name" placeholder="Digite o nome" onChange={valueInput} value={data.name}/>
                     </div>
                     <div className={`${styles.field} ${styles.email}`}>
-                        <input type="text" name="email" placeholder="Digite o email" onChange={valueInput} value={data.email} required/>
+                        <input type="text" name="email" placeholder="Digite o email" onChange={valueInput} value={data.email}/>
                     </div>
                 </div>
 
                 <div className={styles.field}>
-                    <input type="text" name="subject" placeholder="Digite o assunto" onChange={valueInput} value={data.subject} required/>
+                    <input type="text" name="subject" placeholder="Digite o assunto" onChange={valueInput} value={data.subject}/>
                 </div>
 
                 <div className={`${styles.field} ${styles.textarea}`}>
-                    <textarea name="content" cols="30" rows="10" placeholder="Digite o conteúdo" onChange={valueInput} value={data.content} required></textarea>
+                    <textarea name="content" cols="30" rows="10" placeholder="Digite o conteúdo" onChange={valueInput} value={data.content}></textarea>
                 </div>
 
                 <div className={styles.buttonArea}>
